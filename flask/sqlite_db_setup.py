ef9f3484c50f7a69b5afeba74a7728d8f008bd73
Vitala P <113455214+VitalaP@users.noreply.github.com>
2022-11-03 20:38:46 +0530

    Delete flask directory

--- flask/sqlite_db_setup.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn=sqlite3.connect('user.db')
-
-print("Opened Database")
-
-conn.execute('CREATE TABLE user(name TEXT,email TEXT,password TEXT)')
-print('Table created')
-
-conn.close()
